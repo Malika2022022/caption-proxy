eb491d13a0003177ebb4d20d747f36a4100626bb
malikokos <malikokos@MacBook-Air-malikokos.local>
2026-04-15 18:29:06 +0500

    consent cookie

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ function httpsGet(url, headers) {
     }).on('error', reject);
   });
 }
-
 const headers = {
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
@@ -26,6 +25,7 @@ const headers = {
   'Sec-Fetch-Site': 'none',
   'Sec-Fetch-User': '?1',
   'Upgrade-Insecure-Requests': '1',
+  'Cookie': 'CONSENT=YES+cb; YSC=DwKYllHNwuw; VISITOR_INFO1_LIVE=oKckVLH0M4E',
 };
 
 app.get('/raw/:videoId', async (req, res) => {
