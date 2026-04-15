0e95ff5874f06073b26bebf308fedf34e4db4620
malikokos <malikokos@MacBook-Air-malikokos.local>
2026-04-15 18:36:52 +0500

    fix port

--- server.js
+++ server.js
@@ -104,5 +104,6 @@ app.get('/captions/:videoId', async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 3000);
-console.log('Server running on port 3000');
+const PORT = process.env.PORT || 8080;
+app.listen(PORT);
+console.log('Server running on port', PORT);
