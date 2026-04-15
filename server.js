391fbaaffdb96c12979b0c46f7afb5292d56cc1f
malikokos <malikokos@MacBook-Air-malikokos.local>
2026-04-15 19:42:08 +0500

    use playzone transcript with invidious fallback

--- server.js
+++ server.js
@@ -1,39 +1,37 @@
 const express = require('express');
-const { execFile } = require('child_process');
+const { EnhancedYouTubeTranscriptApi } = require('@playzone/youtube-transcript');
 const app = express();
 
-app.get('/captions/:videoId', (req, res) => {
+const api = new EnhancedYouTubeTranscriptApi({}, {
+  enabled: true,
+  instanceUrls: [
+    'https://yewtu.be',
+    'https://invidious.snopyta.org',
+    'https://vid.puffyan.us'
+  ]
+});
+
+app.get('/captions/:videoId', async (req, res) => {
   const videoId = req.params.videoId;
   const lang = req.query.lang || 'en';
   res.setHeader('Access-Control-Allow-Origin', '*');
-
-  execFile('python3', ['-c', `
-from youtube_transcript_api import YouTubeTranscriptApi
-import json
-data = YouTubeTranscriptApi.get_transcript('${videoId}', languages=['${lang}'])
-print(json.dumps(data))
-`], (err, stdout, stderr) => {
-    if (err) return res.status(500).send('Error: ' + stderr);
-    res.setHeader('Content-Type', 'application/json');
-    res.send(stdout);
-  });
+  try {
+    const transcript = await api.fetch(videoId, { languages: [lang, 'en'] });
+    res.json(transcript);
+  } catch(e) {
+    res.status(500).send('Error: ' + e.message);
+  }
 });
 
-app.get('/tracks/:videoId', (req, res) => {
+app.get('/tracks/:videoId', async (req, res) => {
   const videoId = req.params.videoId;
   res.setHeader('Access-Control-Allow-Origin', '*');
-
-  execFile('python3', ['-c', `
-from youtube_transcript_api import YouTubeTranscriptApi
-import json
-tlist = YouTubeTranscriptApi.list_transcripts('${videoId}')
-result = [{'lang': t.language_code, 'name': t.language, 'isAuto': t.is_generated} for t in tlist]
-print(json.dumps(result))
-`], (err, stdout, stderr) => {
-    if (err) return res.status(500).send('Error: ' + stderr);
-    res.setHeader('Content-Type', 'application/json');
-    res.send(stdout);
-  });
+  try {
+    const list = await api.list(videoId);
+    res.json(list);
+  } catch(e) {
+    res.status(500).send('Error: ' + e.message);
+  }
 });
 
 const PORT = process.env.PORT || 8080;
